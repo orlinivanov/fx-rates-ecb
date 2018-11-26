424d65b010fb0348a26c69830319fb5c448df8fe
Orlin Ivanov <mr.orlin.ivanov@gmail.com>
2018-11-26 03:58:57 +0200

    fixed reject message

--- fxRatesECB.js
+++ fxRatesECB.js
@@ -11,7 +11,7 @@ let exchangeRatesECB = (function () {
                 if (request.status === 200) {
                     resolve(request.response);
                 } else {
-                    reject(Error('Data didn\'t load successfully; error code:' + request.statusText));
+                    reject(Error(`Data didn't load successfully; error code: ${request.statusText}`));
                 }
             };
             request.onerror = () => {
